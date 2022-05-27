aad9c6db3daf67ad77d1e070b47ccbaf34de2020
adavis-pk <cubicle.jockey@live.com>
2022-05-26 17:18:44 -0700

    json workflow is working

--- MSRulesEngineSamples/MSRulesEngineSamples/JsonWorkflows.cs
+++ MSRulesEngineSamples/MSRulesEngineSamples/JsonWorkflows.cs
@@ -1,31 +1,46 @@
 ﻿using FluentAssertions;
 using Newtonsoft.Json;
 
+using static System.Console;
+
 namespace MSRulesEngineSamples
 {
     [TestClass]
     public class JsonWorkflows
     {
-        [TestMethod]
-        public async Task UseJsonWorkflow()
+        [DataRow(8750, "TaxRate10Percent")]
+        [DataRow(37812, "TaxRate12Percent")]
+        [DataRow(85000, "TaxRate22Percent")]
+        [DataRow(130999, "TaxRate24Percent")]
+        [DataRow(203567, "TaxRate32Percent")]
+        [DataRow(499345, "TaxRate35Percent")]
+        [DataRow(int.MaxValue, "TaxRate37Percent")]
+        [DataTestMethod]
+        [Description("This work flow is using the default parementer names. i.e: input1, input2, ...etc")]
+        public async Task UseJsonWorkflow(double income, string successfulRule)
         {
-            var jsonWorkflow = await LoadJsonWorkFlows("tax-bracket-workflow.json");
+            //Arrange
+            var jsonWorkflow = await LoadJsonWorkFlows("tax-brackets-single-workflow.json");
             jsonWorkflow.Should().NotBeNullOrWhiteSpace();
 
             string[] jsonConfig = { jsonWorkflow };
             var engine = new RulesEngine.RulesEngine(jsonConfig);
 
-            var inputs = new[]
-            {
-                new { Income = 160500 }
-            };
-
+            //Act
             var workflowNames = engine.GetAllRegisteredWorkflowNames();
+            var input1 = new { Income = income };
+            var results = await engine.ExecuteAllRulesAsync("Tax Brackets", input1);
+
+            //Assert
             workflowNames.Should().NotBeEmpty();
             workflowNames.Count.Should().Be(1);
             workflowNames.Single().Should().Be("Tax Brackets");
 
-            var results = await engine.ExecuteAllRulesAsync("Tax Brackets", default);
+            var passed = results.Single(result => result.Rule.RuleName == successfulRule);
+            passed.IsSuccess.Should().BeTrue();
+
+            var failed = results.Where(result => result.Rule.RuleName != successfulRule);
+            failed.All(result => result.IsSuccess == false).Should().BeTrue();
         }
 
         #region Helper Methods
